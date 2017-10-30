776de14944ccd264982def30cb4bd2e365b6ccd0
William Mcclumpha <william@mcclumpha.uk>
2017-10-30 17:14:39 +0000

    Started work on game controller
    - Begun work on game controller script which will keep track of gameplay
    - Started hooking up UI elements to code

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private int cash = 0;
+    private int mood = 0;
+    private int savings = 0;
+    private int currentDay = 1;
+    private DateTime currentDate = System.DateTime.Now;
+
+    [SerializeField] private Text cashAmountText, moodAmountText, savingsAmountText;
+    [SerializeField] private Text dateText;
+    [SerializeField] private Text eventTitleText;
+    [SerializeField] private Image eventImage;
+    [SerializeField] private Text eventDescriptionText;
+    [SerializeField] private Text leftChoiceButtonText, rightChoiceButtonText;
+
+    private void Start()
+    {
+        dateText.text = GenerateDateText();
+    }
+
+    private string GenerateDateText()
+    {
+        return " <b>Day " + currentDay.ToString() + "/30</b> | " + currentDate.ToString("MMMM d, yyyy");
+    }
 }
