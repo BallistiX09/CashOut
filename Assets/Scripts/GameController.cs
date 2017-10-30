6208354972b9dacd8e1495d03fd0816410a010b8
William Mcclumpha <william@mcclumpha.uk>
2017-10-30 17:17:49 +0000

    Moved UI code into its own script

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,31 +1,11 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-    private int cash = 0;
-    private int mood = 0;
-    private int savings = 0;
-    private int currentDay = 1;
-    private DateTime currentDate = System.DateTime.Now;
-
-    [SerializeField] private Text cashAmountText, moodAmountText, savingsAmountText;
-    [SerializeField] private Text dateText;
-    [SerializeField] private Text eventTitleText;
-    [SerializeField] private Image eventImage;
-    [SerializeField] private Text eventDescriptionText;
-    [SerializeField] private Text leftChoiceButtonText, rightChoiceButtonText;
-
-    private void Start()
-    {
-        dateText.text = GenerateDateText();
-    }
-
-    private string GenerateDateText()
-    {
-        return " <b>Day " + currentDay.ToString() + "/30</b> | " + currentDate.ToString("MMMM d, yyyy");
-    }
+    public static int cash = 0;
+    public static int mood = 0;
+    public static int savings = 0;
+    public static int currentDay = 1;
+    public static DateTime currentDate = System.DateTime.Now;
 }
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private Text cashAmountText, moodAmountText, savingsAmountText;
+    [SerializeField] private Text dateText;
+    [SerializeField] private Text eventTitleText;
+    [SerializeField] private Image eventImage;
+    [SerializeField] private Text eventDescriptionText;
+    [SerializeField] private Text leftChoiceButtonText, rightChoiceButtonText;
+
+    private void Start()
+    {
+        dateText.text = GenerateDateText();
+    }
+
+    private string GenerateDateText()
+    {
+        return " <b>Day " + GameController.currentDay.ToString() + "/30</b> | " + GameController.currentDate.ToString("MMMM d, yyyy");
+    }
 }
